7631e39c008cdc57d26cebc623cec313a6b7a531
takagi <iam.takagi.dev@gmail.com>
2022-03-06 04:21:27 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# takagi.AI.discord
+
+## Install
+`docker-compose.yml`
+```
+version: '3.8'
+
+services:
+  app:
+    image: ghcr.io/iamtakagi/takagi.ai.discord
+    environment:
+      TOKEN: xxx
+    restart: unless-stopped
+```
+
+## LICENSE
+MIT License.
---
+++ app.py
@@ -0,0 +1,35 @@
+import os
+import requests
+
+import discord
+from discord.ext import commands
+from discord_slash import SlashCommand, SlashContext
+
+TOKEN = os.getenv("TOKEN")
+
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+slash = SlashCommand(bot, sync_commands=True)
+
+api_url = 'https://ai.iamtakagi.net/api/make_sentence'
+
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}!")
+
+
+@slash.slash(name='takagi', description='高木が喋ります')
+async def on_command(ctx: SlashContext):
+    if not ctx.guild:
+        return await ctx.send(
+            content="このコマンドは DM で使用できません。",
+            hidden=True
+        )
+    sentence = requests.get(
+        api_url, headers={"content-type": "application/json"}).json()['sentence']
+    await ctx.send(
+        content=sentence, 
+        hidden=True
+    )
+
+bot.run(TOKEN)
